bfdb74c874c461c14523c803251a159d6b37840d
Supreme <supreme.cwy@gmail.com>
2016-12-22 16:24:36 +0800

    add a Binary Tree Level Order Traversal problem

---
+++ src/BinaryTreeLevelOrderTraversal/Solution.java
@@ -0,0 +1,25 @@
+package BinaryTreeLevelOrderTraversal;
+
+import java.util.List;
+
+/**
+ * Created by eason on 2016/12/22.
+ */
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x){
+        val = x;
+    }
+}
+public class Solution {
+
+    public List<List<Integer>> levelOrder(TreeNode root){
+        return null;
+
+
+
+
+    }
+}
---
+++ src/NumberOf1Bits_191/Solution.java
@@ -0,0 +1,11 @@
+package NumberOf1Bits_191;
+
+/**
+ * Created by eason on 2016/12/22.
+ */
+public class Solution {
+    public int hammingWeight(int n){
+        return 0;
+
+    }
+}
