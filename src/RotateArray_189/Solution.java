605980a7087cb76464b31e171b216894ee2247dc
Supreme <supreme.cwy@gmail.com>
2017-01-01 21:53:55 +0800

    add a solution in RotateArray_189 problem

---
+++ src/RotateArray_189/Solution.java
@@ -0,0 +1,39 @@
+package RotateArray_189;
+
+/**
+ * Created by eason on 2017/1/1.
+ */
+
+public class Solution {
+    /**
+     * 最简单的方法
+     *
+     * 将
+     * @param nums
+     * @param k
+     */
+    public void rotate1(int[] nums, int k){
+        int len = nums.length;
+        int[] newNums = new int[len];
+
+        int index = len - k;
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
